74412d244c7c9def53d11488f453d3a8819d723f
Jason Vernot <jason.vernot@gmail.com>
2020-04-25 20:53:24 -0400

    added Employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email, role = "Employee") {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = role;
+    }
+
+    getName = () => this.name;
+
+    getId = () => this.id;
+    
+    getEmail = () => this.email;
+
+    getRole = () => this.role;
+    
+
+}
+
+console.log(this.role);
+module.exports = Employee;
